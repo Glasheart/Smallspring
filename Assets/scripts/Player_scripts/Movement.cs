53e6c300210c2b1cf9762d6e9eac0531ac32d7b1
Jacob Truelove <jet4853@mavs.uta.edu>
2024-09-12 16:23:52 -0500

    fixed issues with musing

--- Assets/scripts/Player_scripts/Movement.cs
+++ Assets/scripts/Player_scripts/Movement.cs
@@ -25,6 +25,28 @@ public class Movement : MonoBehaviour
     {
         if (chest_mode)
         {
+            return;
+        }
+        horzontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        
+        direction = new Vector2(horzontal, vertical);
+
+        if(rb.velocity.magnitude < max_speed)
+            rb.velocity = new Vector3(max_speed * horzontal, max_speed * vertical);
+        else
+        {
+            rb.velocity = new Vector3(max_speed * horzontal * Mathf.Abs(direction.normalized.x), max_speed * vertical * Mathf.Abs(direction.normalized.y));
+        }
+
+        if ((rb.velocity.x < 0 && transform.localScale.x > 0) || (rb.velocity.x > 0 && transform.localScale.x < 0))
+            flip();
+    }
+    private void Update()
+    {
+        if (chest_mode)
+        {
             rb.velocity = new Vector3(0, 0);
             if (Input.GetKeyUp(KeyCode.UpArrow))
             {
@@ -50,27 +72,11 @@ public class Movement : MonoBehaviour
             {
                 slot.vertical_slot = 0;
                 chestUI.SetActive(false);
-                chest_mode = false;  
+                chest_mode = false;
             }
 
             return;
         }
-        horzontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-
-        
-        direction = new Vector2(horzontal, vertical);
-
-        if(rb.velocity.magnitude < max_speed)
-            rb.velocity = new Vector3(max_speed * horzontal, max_speed * vertical);
-        else
-        {
-            rb.velocity = new Vector3(max_speed * horzontal * Mathf.Abs(direction.normalized.x), max_speed * vertical * Mathf.Abs(direction.normalized.y));
-        }
-
-        if ((rb.velocity.x < 0 && transform.localScale.x > 0) || (rb.velocity.x > 0 && transform.localScale.x < 0))
-            flip();
-
         if (Input.GetKeyUp(KeyCode.Q))
         {
             slot.change_highlighted_slot(1);
@@ -80,7 +86,6 @@ public class Movement : MonoBehaviour
             slot.drop_item();
         }
     }
-
     private void flip()
     {
         transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, 1);
