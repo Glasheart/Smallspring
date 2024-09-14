acbfbbb689a76bd963ecb86d39975ec1d2c403d4
Glasheart <110009090+Glasheart@users.noreply.github.com>
2024-09-14 17:21:44 -0500

    fixed a bug I noticed with customer sprites

--- Assets/scripts/CustomerSpawner.cs
+++ Assets/scripts/CustomerSpawner.cs
@@ -6,8 +6,7 @@ public class CustomerSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] customers = new GameObject[5];
     public float[] spawnTimes = new float[5];
-
-    public Item_types desiredDrink;
+    private float temp = 0;
 
     public float countdown = 0;
     // Start is called before the first frame update
@@ -17,7 +16,7 @@ public class CustomerSpawner : MonoBehaviour
         for(int i = 0; i < spawnTimes.Length; i++)
         {
             
-            spawnTimes[i] = Random.Range(120 * (i / (float)customers.Length), 150 * ((i+1) / (float)customers.Length));
+            spawnTimes[i] = Random.Range(120 * (i / (float)customers.Length), 150 * ((i) / (float)customers.Length));
             if (i == 0)
             {
                 spawnTimes[i] = 5;
@@ -25,7 +24,13 @@ public class CustomerSpawner : MonoBehaviour
         }
         for(int i = 0; i < spawnTimes.Length - 1;i++)
         {
-            if (spawnTimes[i] + 5 > spawnTimes[i+1] && spawnTimes[i] > spawnTimes[i+1])
+            if (spawnTimes[i] > spawnTimes[i+1])
+            {
+                temp = spawnTimes[i];
+                spawnTimes[i] = spawnTimes[i+1];
+                spawnTimes[i + 1] = temp;
+            }
+            if (spawnTimes[i] + 5 > spawnTimes[i+1]) //if there isn't a 5 second cooldown, add one.
             {
                 spawnTimes[i + 1] += 5;
             }
